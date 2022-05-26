94807a8fdce3601386e80eb6f11e306edb418fac
sylvey <80093140+sylvey@users.noreply.github.com>
2022-05-26 14:28:57 +0800

    Update List.js

--- api/List.js
+++ api/List.js
@@ -1,10 +1,10 @@
 import FoodType from "../hardData/FoodType";
 import ListData from "../hardData/ListData";
 
-export const getListData = async (tag, keyword) =>{
+export const getListData = async (tag, keyword, startDate, endDate) =>{
     return ListData; // 單次回傳 N 個
 }
 
-export const getFoodType = async(searchWord)=>{ // searchword == "" return all foodType
+export const getFoodType = async()=>{ // searchword == "" return all foodType
     return FoodType;
 }
