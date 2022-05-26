5e6b9d45bc031cb7f3235e2b780686e0eabb2d4b
sylvey <80093140+sylvey@users.noreply.github.com>
2022-05-26 14:29:54 +0800

    Update Home.js

--- api/Home.js
+++ api/Home.js
@@ -1,6 +1,6 @@
 import DailyData from "../hardData/DailyData"
 
-export const getDailyData = async (keyword) =>{
+export const getDailyData = async (keyword, startDay, endDay) =>{
     return DailyData; // 單次回傳 N 個
 }
 
